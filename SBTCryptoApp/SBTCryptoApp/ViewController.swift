6c943f1865e666e266132f38192a40b64aca5422
Yefga <yefga.tp@icloud.com>
2021-07-07 14:05:52 +0700

    initialize project

---
+++ SBTCryptoApp/SBTCryptoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SBTCryptoApp
+//
+//  Created by Yefga on 07/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
