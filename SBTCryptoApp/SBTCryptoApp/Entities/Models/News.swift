586f01bacd9ff0b692f8564d5422193100aa29d0
Yefga <yefga.tp@icloud.com>
2021-07-11 16:58:10 +0700

    adding latest news modules and news entity, moving cryptocurrency entity to models

---
+++ SBTCryptoApp/SBTCryptoApp/Entities/Models/News.swift
@@ -0,0 +1,8 @@
+//
+//  News.swift
+//  SBTCryptoApp
+//
+//  Created by Yefga on 11/07/21.
+//
+
+import Foundation
---
+++ SBTCryptoApp/SBTCryptoApp/Modules/LatestNews/LatestNewsRouter.swift
@@ -0,0 +1,64 @@
+/// Copyright © 2021. SBTCryptoApp. All rights reserved.
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+///
+///  LatestNewsRouter.swift
+///  SBTCryptoApp
+///
+///  Created by Yefga on 11/07/21.
+///  Using Swift 5.0
+///  Running on macOS 11.4
+///
+
+import UIKit
+
+class LatestNewsRouter: LatestNewsPresenterToRouterProtocol {
+
+    static let shared = LatestNewsRouter()
+    
+    func createModule() -> LatestNewsViewController {
+        
+        let view = LatestNewsViewController()
+        
+        let presenter: LatestNewsViewToPresenterProtocol & LatestNewsInteractorToPresenterProtocol = LatestNewsPresenter()
+        let interactor: LatestNewsPresenterToInteractorProtocol = LatestNewsInteractor()
+        let router: LatestNewsPresenterToRouterProtocol = LatestNewsRouter()
+        
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        
+        return view
+    }
+    
+    func goTo(viewController: LatestNewsViewController) {
+        
+    }
+}
