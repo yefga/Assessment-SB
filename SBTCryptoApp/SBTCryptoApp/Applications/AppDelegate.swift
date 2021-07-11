fdd42ec2dffa2cb8e88651b1b7a0875bdf9e5f8d
Yefga <yefga.tp@icloud.com>
2021-07-11 15:25:54 +0700

    adding topMostViewController helper

--- SBTCryptoApp/SBTCryptoApp/Applications/AppDelegate.swift
+++ SBTCryptoApp/SBTCryptoApp/Applications/AppDelegate.swift
@@ -10,7 +10,7 @@ import AlamofireNetworkActivityLogger
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -23,16 +23,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let frame = UIScreen.main.bounds
         window = UIWindow.init(frame: frame)
-
+        
         let vc = TopListsRouter.shared.createModule()
         let nav = UINavigationController(rootViewController: vc)
         window?.rootViewController = nav
-
+        
         window?.makeKeyAndVisible()
-
+        
         return true
     }
-
-
+    
+    
 }
 
+extension UIViewController {
+    func topmostViewController() -> UIViewController {
+        if let navigationVC = self as? UINavigationController,
+           let topVC = navigationVC.topViewController {
+            return topVC.topmostViewController()
+        }
+        if let tabBarVC = self as? UITabBarController,
+           let selectedVC = tabBarVC.selectedViewController {
+            return selectedVC.topmostViewController()
+        }
+        if let presentedVC = presentedViewController {
+            return presentedVC.topmostViewController()
+        }
+        if let childVC = children.last {
+            return childVC.topmostViewController()
+        }
+        return self
+    }
+}
+
+extension UIApplication {
+    func topmostViewController() -> UIViewController? {
+        return windows.filter {$0.isKeyWindow}.first?.rootViewController?.topmostViewController()
+    }
+}
