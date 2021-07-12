59f3413f095f57c767f90429ef020e17f51d24b8
Yefga <yefga.tp@icloud.com>
2021-07-12 14:23:02 +0700

    adding constant pageRequest

--- SBTCryptoApp/SBTCryptoApp/Configs/Constant.swift
+++ SBTCryptoApp/SBTCryptoApp/Configs/Constant.swift
@@ -9,4 +9,5 @@ import Foundation
 
 public let APIKey = "092985fd33c3fa60be0ea75dfa3079d84a69cb81afa1d8507d38c5313ec81c3f"
 public let minimumLimitRequest = 10
+public let pageRequest = 1
 public let currencyRequest: String = "USD"
