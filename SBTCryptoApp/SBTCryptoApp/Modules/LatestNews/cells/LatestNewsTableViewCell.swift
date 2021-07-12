d2d029c0e5c7e36abfde7b15343424775b8d6c30
Yefga <yefga.tp@icloud.com>
2021-07-12 15:08:22 +0700

    adding new table cell for latest news

---
+++ SBTCryptoApp/SBTCryptoApp/Modules/LatestNews/cells/LatestNewsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  LatestNewsTableViewCell.swift
+//  SBTCryptoApp
+//
+//  Created by Yefga on 12/07/21.
+//
+
+import UIKit
+
+class LatestNewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var publisherLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+}
